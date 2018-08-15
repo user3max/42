d628303e65f25103f21a2ead1ad6bf62baa995f3
user3max <42212845+user3max@users.noreply.github.com>
2018-08-15 10:44:55 +0300

    Update main.cpp

--- Piscine_CPP/Day01/ex00/main.cpp
+++ Piscine_CPP/Day01/ex00/main.cpp
@@ -4,7 +4,6 @@ void ponyOnTheHeap(void)
 {
 	std::string name;
 	Pony* pny1 = new Pony;
-	//std::string num = "first";
 
 	pny1->setNr("first");
 	std::cout << "Give a name for your " << pny1->getNr() << pny1->getColor() << " pony" << std::endl;
@@ -18,7 +17,6 @@ void ponyOnTheStack(void)
 {
 	std::string name;
 	Pony pny2;
-	//std::strin num = "second";
 	
 	pny2.setNr("second");
 	std::cout << "Give a name for your " << pny2.getNr() << pny2.getColor() << " pony" << std::endl;

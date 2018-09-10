33d0b3a4a6d1473a713624257c7ecbf1c6816df9
user3max <maxymyn@gmail.com>
2018-09-10 14:59:45 +0300

    ex01

--- Piscine_CPP/Day02/ex01/main.cpp
+++ Piscine_CPP/Day02/ex01/main.cpp
@@ -1,6 +1,5 @@
 //main.cpp by Carp-Bezverhnii Maxim
 #include "Fixed.hpp"
-#include <iostream>
 
 int main(void)
 {
@@ -20,6 +19,6 @@ int main(void)
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-	
+
 	return 0;
 }
